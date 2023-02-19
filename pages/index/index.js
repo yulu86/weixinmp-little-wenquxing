58462f4b8a093bb6a7fae25be36313fb26926650
Yulu Xu <xuyulu86@126.com>
2023-02-19 18:01:25 +0800

    列表demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,5 +8,11 @@ Page({
     var2: undefined, // undefined的变量不会显示
     var3: null,
     var4: "var4",
+    length: 4,
+    books: [
+        {name: "hello", price: 30.2},
+        {name: "world", price: 73.2},
+        {name: "大鸡鸡", price: 53.1}
+    ],
   },
 })
